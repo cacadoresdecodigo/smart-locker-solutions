5c790b759d6822062b2b77c51ddf7133701302e9
Bento Luiz <bentoluizv@gmail.com>
2023-07-12 14:40:51 -0300

    refatorado funcao de cadastrar usuario

--- src/features/cadastrarUsuario.js
+++ src/features/cadastrarUsuario.js
@@ -1,49 +1,23 @@
-function pegarDadosFormCadastro() {
-  let razaoSocial = document.getElementById("inpt_razaosocial").value
-  let nomeFantasia = document.getElementById("inpt_nomefantasia").value
-  let cnpj = document.getElementById("inpt_cnpj").value
-  let telefone = document.getElementById("inpt_telefone").value
-  let email = document.getElementById("inpt_email").value
-  let senha = document.getElementById("inpt_senha").value
+import { pegarDadosForm } from "./pegarDadosForm.js";
+import { sync, load, create } from "../data/localStorage/empresas.js";
 
-  return {
-      razaoSocial,
-      nomeFantasia,
-      cnpj,
-      telefone,
-      email,
-      senha
-  }
-}
+const empresas = sync();
+console.log(empresas);
 
-let empresas = localStorage.getItem("empresas");
- 
-if (!empresas) {
-    empresas = [];
-  } else {
-    empresas = JSON.parse(empresas);
+const form = document.getElementById("form_cadastro");
+
+form.addEventListener("submit", (e) => {
+  const dadosForm = pegarDadosForm(form);
+
+  const emailExistente = empresas.find((empresa) => empresa.email === dadosForm.email);
+  const cnpjExistente = empresas.find((empresa) => empresa.cnpj === dadosForm.cnpj);
+
+  if (emailExistente || cnpjExistente) {
+    alert("Usuário com mesmo cnpj ou e-mail já cadastrado");
+    return;
   }
 
-function cadastrar() {
-  const dadosEmpresa = pegarDadosFormCadastro()
+  create(empresas, dadosForm);
 
-  const inputsValues = Object.values(dadosEmpresa)
-  
-  for (let i = 0; i < inputsValues.length; i++) {
-    if (inputsValues[i] === "") {
-      alert("ERRO! Confira os dados digitados")
-      return
-    }
-  }
-
-  const jaExiste = empresas.find((empresa) => empresa.email === inputsValues[4])
-  if (jaExiste) {
-    alert("E-mail já cadastrado")
-    return
-  }
-
-  empresas.push(dadosEmpresa)
-  localStorage.setItem("empresas", JSON.stringify(empresas));
-  window.location.href = "/src/pages/index/index.html"
-}
-
+  load(empresas);
+});
