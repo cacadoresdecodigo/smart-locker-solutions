0ab1f33fa934c135fd86ac35055c8abadd4cd0c0
Bento Luiz <bentoluizv@gmail.com>
2023-07-12 14:40:36 -0300

    funcionalidade para pegar dados de um form

---
+++ src/features/pegarDadosForm.js
@@ -0,0 +1,15 @@
+export function pegarDadosForm(form) {
+  if (!form instanceof HTMLFormElement) throw new Error("Elemento não é um formulário");
+
+  const inputs = Array.from(form.elements);
+
+  const dadosForm = {};
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i] instanceof HTMLInputElement) {
+      dadosForm[inputs[i].labels[0].htmlFor] = inputs[i].value;
+    }
+  }
+
+  return dadosForm;
+}
