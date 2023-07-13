b8bf006d8ecb89bdcc50c29008427798d6e07744
Bento Luiz <bentoluizv@gmail.com>
2023-07-13 16:23:26 -0300

    ajustes login

--- src/script/login.js
+++ src/script/login.js
@@ -6,12 +6,14 @@ let senhaLogado = document.getElementById("inputSenha");
 let empresas = sync();
 
 let botaoSubmit = document.getElementById("submit_login");
+console.log(empresas);
 
 botaoSubmit.addEventListener("click", () => {
   for (let i = 0; i < empresas.length; i++) {
     if (emailLogado.value == empresas[i].email && senhaLogado.value == empresas[i].senha) {
       let userLogado = emailLogado.value;
       localStorage.setItem("userLogado", JSON.stringify(userLogado));
+      console.log(userLogado, JSON.parse(localStorage.getItem("userLogado")));
       window.location.href = "./index.html";
     }
   }
