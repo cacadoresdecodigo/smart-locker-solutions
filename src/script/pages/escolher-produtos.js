c58685372c142f00680fcf2cb8fc3b447dc8af76
Bento Luiz <bentoluizv@gmail.com>
2023-07-17 22:21:34 -0300

    navegacao area da empresa

---
+++ src/script/pages/escolher-produtos.js
@@ -0,0 +1,22 @@
+import { autorizar } from "../auth/autorizar.js";
+import Select from "../components/Select.js";
+
+const baseUrl = "http://127.0.0.1:5500/src/pages";
+const section = autorizar();
+console.log(section);
+
+if (section) {
+  const loginBtn = document.getElementById("btn_login");
+  loginBtn.hidden = true;
+  Select(
+    [`${section.email.split("@")[0]}`, "Editar Cadastro", "Escolher Locker"],
+    (selected) => {
+      if (selected === "Editar Cadastro") {
+        window.location.href = baseUrl + "/editar-cadastro.html";
+      }
+      if (selected === "Escolher Locker") {
+        window.location.href = baseUrl + "/escolher-produtos.html";
+      }
+    }
+  );
+}
