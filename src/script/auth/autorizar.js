c14266ef7db6719055403f9c54b68369ece2a8e6
Bento Luiz <bentoluizv@gmail.com>
2023-07-17 22:22:08 -0300

    algoritmo para autorizar usuario

--- src/script/auth/autorizar.js
+++ src/script/auth/autorizar.js
@@ -2,6 +2,7 @@ import { ehAdmin } from "../utils/ehAdmin.js";
 
 export function autorizar() {
   const paginaAtual = window.location.href;
+  const baseUrl = "http://127.0.0.1:5500/src/pages";
 
   let path = paginaAtual
     .slice(paginaAtual.lastIndexOf("/"))
@@ -11,39 +12,43 @@ export function autorizar() {
   const session = JSON.parse(sessionData);
 
   if (!session) {
-    const permissoes = ["/", "/cadastro", "/produtos", "/sobre", "/login"];
-    if (permissoes.includes(path)) {
-      console.log("tem permissão");
-    } else {
-      console.log("não tem permissão");
+    const permissoes = ["/index", "/cadastro", "/produtos", "/sobre", "/login"];
+    if (!permissoes.includes(path)) {
+      //   window.location.href = baseUrl + "/login.html";
     }
   }
 
   if (session) {
     const admin = ehAdmin(session.email);
+
     if (admin) {
-      const permissoes = ["/", "/cadastro", "/produtos", "/sobre", "/admin"];
-      if (permissoes.includes(path)) {
-        console.log("tem permissão");
-      } else {
-        console.log("não tem permissão");
+      const permissoes = [
+        "/index",
+        "/cadastro",
+        "/produtos",
+        "/sobre",
+        "/admin",
+      ];
+      if (!permissoes.includes(path)) {
+        window.location.href = baseUrl + "/";
       }
+
+      return session;
     }
 
     if (!admin) {
       const permissoes = [
-        "/",
+        "/index",
         "/cadastro",
         "/produtos",
         "/sobre",
         "/editar-cadastro",
-        "escolher-produtos",
+        "/escolher-produtos",
       ];
-      if (permissoes.includes(path)) {
-        console.log("tem permissão");
-      } else {
-        console.log("não tem permissão");
+      if (!permissoes.includes(path)) {
+        window.location.href = baseUrl + "/";
       }
+      return session;
     }
   }
 }
